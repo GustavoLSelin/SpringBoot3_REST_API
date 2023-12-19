ef2f4c5bd0a75e537230be2d76b796df478fe408
Gustavo Lopes Selin <gustavolopesselin@gmail.com>
2023-12-18 21:02:42 -0300

    Continuacao do curso 18/12/2023

---
+++ src/main/java/com/exemplo/SpringBoot3_REST_API/CorsConfiguration.java
@@ -0,0 +1,22 @@
+package com.exemplo.SpringBoot3_REST_API;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*
+    LOPES - Esse á a classe responsavel por efetuar a correção dos CORs dentro dos navegadores
+
+ */
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+
+}
+
--- src/main/java/com/exemplo/SpringBoot3_REST_API/controller/MedicoController.java
+++ src/main/java/com/exemplo/SpringBoot3_REST_API/controller/MedicoController.java
@@ -1,15 +1,35 @@
 package com.exemplo.SpringBoot3_REST_API.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
 
+
+
+    @GetMapping
+    public String buscarMedico() {
+
+        return "{\n" +
+                "\"nome\": \"Rodrigo Ferreira\",\n" +
+                "\"email\": \"rodrigo.ferreira@voll.med\",\n" +
+                "\"crm\": \"123456\",\n" +
+                "\"especialidade\": \"ortopedia\",\n" +
+                "\"endereco\": {\n" +
+                "    \"logradouro\": \"rua 1\",\n" +
+                "    \"bairro\": \"bairro\",\n" +
+                "    \"cep\": \"12345678\",\n" +
+                "    \"cidade\": \"Brasilia\",\n" +
+                "    \"uf\": \"DF\",\n" +
+                "    \"numero\": \"1\",\n" +
+                "    \"complemento\": \"complemento\"\n" +
+                "    }\n" +
+                "}";
+    }
     @PostMapping
-    public String cadastroMedico() {
+    public String cadastroMedico(@RequestBody String json) {
 
         return "Medico cadastrado com sucesso";
 
